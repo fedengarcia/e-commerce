9af4b3a5aef2ca9eeb530a7fd85d99a86a88c79b
fedengarcia <fedengarcia11@hotmail.com>
2021-08-06 07:47:28 -0300

    ErrorFormDialog

--- src/components/DialogContainer/DialogContainer.js
+++ src/components/DialogContainer/DialogContainer.js
@@ -1,22 +1,20 @@
 import React,{useState, useContext, useEffect} from 'react';
 import {ModeContext} from '../../Context/CartContext';
-import Form from '../Form/Form';
 import {useParams} from 'react-router-dom';
 import DialogComponent from '../DialogoComponent/DialogComponent';
 import CountValidationDialog from '../DialogoComponent/CountValidationDialog';
 import TrashDialog from '../DialogoComponent/TrashDialog';
 import CleanCartDialog from '../DialogoComponent/CleanCartDialog';
+import FormDialogError from '../DialogoComponent/FormDialogError';
 
 export default function DialogContainer () { 
     const [openTrashDialog, setOpenTrashDialog] = useState(false);
-    const [openFormDialog, setOpenFormDialog] = useState (false);
+    const [openFormDialogError, setOpenFormDialogError] = useState (false);
     const [openConfirmBuyDialog,setOpenConfirmBuyDialog] = useState(false);
     const [openCleanCartDialog, setOpenCleanCartDialog] = useState (false);
     const [openCountValidationDialog,setOpenCountValidationDialog] = useState(false);
     const [idCompra,setIdCompra] = useState(undefined);
-
-    const {removeItem,clear,newOrder,itemTrashId} = useContext(ModeContext);
-
+    const {removeItem,clear,itemTrashId} = useContext(ModeContext);
     const {dialogType} = useParams();
 
     useEffect(() => {
@@ -31,7 +29,7 @@ export default function DialogContainer () {
                 setOpenCountValidationDialog(true);
             }
             if(dialogType === "formDialog"){
-                setOpenFormDialog(true);
+                setOpenFormDialogError(true);
             }
             if(dialogType === "endBuyDialog"){
                 setOpenConfirmBuyDialog(true);
@@ -40,25 +38,6 @@ export default function DialogContainer () {
     }, [dialogType]);
 
 
-
-    const renderFormDialog = (openFormDialog,newOrder) => {
-        return <DialogComponent
-        open={openFormDialog}
-        openDialog={setOpenFormDialog}
-        closeDialog={()=> setOpenFormDialog(false)}
-        handleConfirm={()=> setOpenFormDialog(false)}
-        handleEndBuying={()=> setOpenConfirmBuyDialog(true)}
-        title="Completa el formulario para continuar"
-        firstButton="Cancelar"
-        secondButton="Comprar"
-        newOrder = {newOrder}
-        clearCart={() => clear()}
-        setIdCompra={(idCompra) => setIdCompra(idCompra)}
-
-        >
-            <Form/>
-        </DialogComponent>
-    }
 
     const renderConfirmBuyDialog = (openConfirmBuyDialog,idCompra) => {
         return <DialogComponent
@@ -77,9 +56,10 @@ export default function DialogContainer () {
 
     
     return <>
+
         {openTrashDialog && TrashDialog({openTrashDialog,itemTrashId,removeItem,setOpenTrashDialog})}
         {openCleanCartDialog && CleanCartDialog({openCleanCartDialog,setOpenCleanCartDialog,clear})}
-        {openFormDialog && renderFormDialog({openFormDialog,newOrder})}
+        {openFormDialogError && FormDialogError({openFormDialogError,setOpenFormDialogError})}
         {openConfirmBuyDialog && renderConfirmBuyDialog({openConfirmBuyDialog,idCompra})}
         {openCountValidationDialog && CountValidationDialog ({openCountValidationDialog,setOpenCountValidationDialog})}
 
